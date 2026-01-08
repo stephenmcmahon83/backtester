ed03fc2ef7c847c7d303063817690460ba4002f6
stephenmcmahon83 <stephenmcmahon83@gmail.com>
2026-01-08 18:23:01 -0500

    Daily update - Thu 01/08/2026

---
+++ daily_update.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d C:\Users\smcma\OneDrive\Desktop\seasonal10302025\my-stock-app\
+
+call python populate_calendar.py
+call python download_stock_data.py
+
+git add .
+git commit -m "Daily update - %date%"
+git push origin main
